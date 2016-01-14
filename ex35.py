f51608fc2cd636fc21d6d0511e5b2baaab888abd
kevyang1004 <kevinyang@seoulchristian.org>
2016-01-14 15:30:46 +0900

    Finished exercise 36

--- ex35.py
+++ ex35.py
@@ -4,14 +4,17 @@ def gold_room():
     print "This room is full of gold. How much do you take?"
 
     choice = raw_input("> ")
+
     if "0" in choice or "1" in choice:
         how_much = int(choice)
+
     else:
         dead("Man, learn to type a number.")
 
     if how_much < 50:
         print "Nice, you're not greedy, you win!"
         exit(0)
+
     else:
         dead("You greedy bastard!")
 
@@ -28,13 +31,17 @@ def bear_room():
 
         if choice == "take honey":
             dead("The bear looks at you then slaps your face off.")
+
         elif choice == "taunt bear" and not bear_moved:
             print "The bear has moved from the door. You can go through it now."
             bear_moved = True
+
         elif choice == "taunt bear" and bear_moved:
             dead("The bear gets pissed off and chews your leg off.")
+
         elif choice == "open door" and bear_moved:
             gold_room()
+
         else:
             print "I got no idea what that means."
 
@@ -48,8 +55,10 @@ def cthulhu_room():
 
     if "flee" in choice:
         start()
+
     elif "head" in choice:
         dead("Well that was tasty!")
+
     else:
         cthulhu_room()
 
@@ -67,8 +76,10 @@ def start():
 
     if choice == "left":
         bear_room()
+
     elif choice == "right":
         cthulhu_room()
+
     else:
         dead("You stumble around the room until you starve.")
 
