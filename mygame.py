0c1cc558e7b256e9632de826e794fb74f3e480c5
kevyang1004 <kevinyang@seoulchristian.org>
2016-01-20 22:08:19 +0900

    exercise 36

---
+++ mygame.py
@@ -0,0 +1,46 @@
+from sys import exit
+
+def in_front_of_SCS_building():
+    print "You are in front of SCS building."
+
+    choice = raw_input("Do you want to go in the bilding?")
+
+    if choice "Yes":
+        print "welcome to SCS"
+
+    else choice "No":
+        dead("You just got all F from your grades")
+
+
+def 1st_floor():
+    print "There is the way to go the office"
+    print "There is the way to go to the basement"
+    print "There is the way to go to 2nd floor"
+
+    choice = raw_input("Where do you want to go?")
+
+    if choice "go to the office":
+        print "No one is there"
+
+    elif choice "go to the basement":
+        print "All the SCS students and faculties are in chapel"
+
+    elif choice "go to the 2nd floor":
+        print "There is the elementary school"
+
+    else choice "get out of the building":
+        dead("You're kicked out of school!")
+
+def office():
+    print "Suddenly, two dogs are trying to attack you"
+
+    choice = raw_input("What would you do with them?")
+
+    if choice "run away":
+        print "You just survived from the dogs' attack"
+
+    else choice "fight":
+        dead("They bite you")
+
+def basement():
+    
